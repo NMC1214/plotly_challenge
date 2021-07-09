28935821edc36056f39f117b1285876555d2ce70
Nicole <nclotus11@gmail.com>
2021-07-09 17:42:05 -0400

    commit -m "drop down and box data"

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
+var names;
+var metadata;
+var samples;
 
 // Fetch the JSON data and console log it
 d3.json("samples.json").then(function(data) {
-    console.log(data);
+    console.log(data.names);
+    names = data.names;
+    metadata = data.metadata[0];
+    samples = data.samples;
+
+    var dropdownMenu = d3.select("#selDataset");
+
+    names.forEach(element => dropdownMenu.append("option").text(element).property("value", element));
+
+    var boxinfo = d3.select('#sample-metadata');
+
+    Object.keys(metadata).forEach(key => {
+      console.log(key, metadata[key]);
+      boxinfo.append("h6").text(`${key}: ${metadata[key]}`);
+    });
+
+
   });
 
-
-data.map(bbdata);
-
-function bbdata(details) {
-  return = [details.metadata];
+function optionChanged(variable) {
+  console.log(variable);
+  //call to function to build panel table
 }
+  
+// data.map(bbdata);
+
+// function bbdata(details) {
+//   return = [details.metadata];
+// }
      //create array from values in data?
     // var ID = Object.values(data.id);
     // var wfreq = Object.values(data.ethnicity);
